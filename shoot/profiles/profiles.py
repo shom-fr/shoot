8c1b60f3d9c96c09cebb59c3cc2de3d2dc47a7c0
Jean-Baptiste ROUSTAN <jbrousta@datarmor2.ib0.ice.ifremer.fr>
2026-01-05 10:36:22 +0000

    modified profile for argopy

--- shoot/profiles/profiles.py
+++ shoot/profiles/profiles.py
@@ -8,31 +8,15 @@ Created on Thu Aug  19 10:20:12 2025
 
 import os, glob
 import functools
-from .download import load, Download
+from .download import Download,  load_from_ds
 import xarray as xr
 import numpy as np
 from .. import cf as scf
 
 
-def determine_region(lat_min, lat_max, lon_min, lon_max):
-    REGIONS = Download.get_regions()
-    for region, bounds in REGIONS.items():
-        lat_bounds = bounds["lat"]
-        lon_bounds = bounds["lon"]
-        # check if dataset included in a region
-        if (
-            lat_min >= lat_bounds[0]
-            and lat_max <= lat_bounds[1]
-            and lon_min >= lon_bounds[0]
-            and lon_max <= lon_bounds[1]
-        ):
-            return region
-    return "global"
-
-
 class Profile:
     def __init__(self, prf):
-        self.time = np.datetime64("1950-01-01") + np.timedelta64(int(prf.TIME.values * 86400), "s")
+        self.time = prf.TIME.values
         self.lat = prf.LATITUDE.values
         self.lon = prf.LONGITUDE.values
         self.depth = np.arange(1, 2001)
@@ -54,52 +38,21 @@ class Profile:
 
 
 class Profiles:
-    def __init__(self, time, region, root_path, data_types, download=True):
-        self.root_path = os.path.join(root_path, region)
-        self.region = region
+    def __init__(self, time, root_path, brut_prf):
+        self.root_path = root_path
         self.time = time
         self.years = np.arange(self.time.min().dt.year.values, self.time.max().dt.year.values + 1)
-        self.months = [
-            self.time.min().dt.month.values,
-            self.time.max().dt.month.values,
-        ]
-
-        if download:
-            load(
-                self.years,
-                self.months,
-                self.region,
-                self.root_path,
-                data_types,
-            )
-
-        else:
-            print("Data already exists")
-
-        file_path = []
-        for year in self.years:
-            file_path += sorted(glob.glob(os.path.join(self.root_path, str(year), "*.nc")))
 
         self.profiles = []
-        for f in file_path:
-            tmp_profiles = xr.open_dataset(f, decode_times=False)
-            for i in tmp_profiles.N_PROF:
-                prf = Profile(tmp_profiles.isel(N_PROF=i))
-                if prf.valid:
-                    self.profiles.append(prf)
+        for i in brut_prf.N_PROF:
+            prf = Profile(brut_prf.isel(N_PROF=i))
+            if prf.valid:
+                self.profiles.append(prf)
 
     @classmethod
-    def from_ds(cls, ds, root_path, region=None, data_types=["XT", "PF"], download=True):
-
-        if not region:
-            lat_min = scf.get_lat(ds).min().values
-            lat_max = scf.get_lat(ds).max().values
-            lon_min = scf.get_lon(ds).min().values
-            lon_max = scf.get_lon(ds).max().values
-            region = determine_region(lat_min, lat_max, lon_min, lon_max)
-            print("Selected region : %s" % region)
-
-        return cls(ds.time, region, root_path, data_types, download=download)
+    def from_ds(cls, ds, root_path):
+        brut_prf = load_from_ds(ds, root_path)
+        return cls(ds.time, root_path, brut_prf)
 
     @functools.cached_property
     def ds(self):
@@ -162,4 +115,5 @@ class Profiles:
                     if b:
                         e.p_id.append(prf.isel(profil=i).p_id.values)
                         eddy_pos[prf.isel(profil=i).p_id.values] = e.track_id
-        self.ds = self.ds.assign(eddy_pos=("profil", eddy_pos))
+        self.ds = self.ds.assign(eddy_pos=("profil", np.array(eddy_pos, dtype = np.int32)))
+
